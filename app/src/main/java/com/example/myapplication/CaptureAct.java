88a3b26393473f1779a9618eaa1ad3c078e1c6c0
francineliahn <94122081+francineliahn@users.noreply.github.com>
2022-08-17 14:32:56 +0800

    camera with a smile and torotot sent with fire effects

---
+++ app/src/main/java/com/example/myapplication/CaptureAct.java
@@ -0,0 +1,4 @@
+package com.example.myapplication;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton cameraBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraBtn = findViewById(R.id.camera_png);
+        cameraBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        scanCode();
+    }
+
+    private void scanCode(){
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+
     }
 }
